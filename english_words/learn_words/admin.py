cbddcbc47bc9de8187605e4da6be00d58bdddae2
Smirnov Denis <denis.smirnov@intel.com>
2016-12-05 16:51:06 +0300

    Created DBs for English and Russia words

--- english_words/learn_words/admin.py
+++ english_words/learn_words/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EnglishWord
+from .models import RussianWord
+
+admin.site.register(EnglishWord)
+admin.site.register(RussianWord)
--- english_words/learn_words/models.py
+++ english_words/learn_words/models.py
@@ -4,15 +4,15 @@ from django.conf import settings
 from django.db import models
 
 
-class English(models.Model):
+class EnglishWord(models.Model):
     word = models.CharField(max_length=64)
     translation = models.CharField(max_length=256)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    rate = models.FloatField(default=0.0)
+    rate = models.IntegerField(default=0)
 
 
-class Russian(models.Model):
+class RussianWord(models.Model):
     word = models.CharField(max_length=64)
     translation = models.CharField(max_length=256)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    rate = models.FloatField(default=0.0)
+    rate = models.IntegerField(default=0)
