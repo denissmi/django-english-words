c99d0398aeb12918ff142b627cc700c8bf677840
Smirnov Denis <denis.smirnov@intel.com>
2016-12-26 18:00:07 +0300

    Added inspection that user is not authorized when showing words

--- english_words/learn_words/urls.py
+++ english_words/learn_words/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from learn_words import views
 
 urlpatterns = [
-    url(r'^show/english/$', views.show_english),
-    url(r'^show/russian/$', views.show_russian),
+    url(r'^show/english/$', views.show_english, name='show_english'),
+    url(r'^show/russian/$', views.show_russian, name='show_russian'),
 ]
--- english_words/learn_words/views.py
+++ english_words/learn_words/views.py
@@ -5,25 +5,28 @@ from django.shortcuts import render
 from .models import EnglishWord, RussianWord
 
 
-def show_english(request):
-    try:
-        user = User.objects.get(username=request.user.username)
-        word_list = EnglishWord.objects.filter(author=user.id)
-    except ObjectDoesNotExist:
-        raise Http404("English Words do not exist")
+def show_words(request, model_name):
     context = {
-        'word_list': word_list,
+        'word_list': None,
+        'warning_message': None
     }
+    try:
+        try:
+            user = User.objects.get(username=request.user.username)
+        except ObjectDoesNotExist:
+            context['warning_message'] = 'You are not authorized.'
+            return render(request, 'show_words.html', context)
+        word_list = model_name.objects.filter(author=user.id)
+    except ObjectDoesNotExist:
+        raise Http404("Words do not exist")
+    context['word_list'] = word_list
+    context['warning_message'] = 'No words are available.'
     return render(request, 'show_words.html', context)
+
+
+def show_english(request):
+    return show_words(request, EnglishWord)
 
 
 def show_russian(request):
-    try:
-        user = User.objects.get(username=request.user.username)
-        word_list = RussianWord.objects.filter(author=user.id)
-    except ObjectDoesNotExist:
-        raise Http404("Russian Words do not exist")
-    context = {
-        'word_list': word_list,
-    }
-    return render(request, 'show_words.html', context)
+    return show_words(request, RussianWord)
