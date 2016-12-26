1e46df8e550b598742d7c66ebc372c9a8234ab27
Smirnov Denis <denis.yu.smirnov@mail.ru>
2016-12-26 18:35:27 +0300

    Limited filling of some table columns by Admin

--- english_words/learn_words/admin.py
+++ english_words/learn_words/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import EnglishWord, RussianWord
 
-admin.site.register(EnglishWord)
-admin.site.register(RussianWord)
+
+class AdminEnglishWord(admin.ModelAdmin):
+    fields = ['word', 'translation', 'author']
+
+
+class AdminRussianWord(admin.ModelAdmin):
+    fields = ['word', 'translation', 'author']
+
+admin.site.register(EnglishWord, AdminEnglishWord)
+admin.site.register(RussianWord, AdminRussianWord)
