cfd13c73aa8cc4c970569825fd9a4366544864ae
Smirnov Denis <denis.smirnov@intel.com>
2016-12-26 15:16:57 +0300

    Added words filter by username

--- english_words/learn_words/views.py
+++ english_words/learn_words/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.shortcuts import render
 from .models import EnglishWord, RussianWord
 
 
 def show_english(request):
     try:
-        word_list = EnglishWord.objects.all()
+        user = User.objects.get(username=request.user.username)
+        word_list = EnglishWord.objects.filter(author=user.id)
     except ObjectDoesNotExist:
         raise Http404("English Words do not exist")
     context = {
@@ -17,7 +19,8 @@ def show_english(request):
 
 def show_russian(request):
     try:
-        word_list = RussianWord.objects.all()
+        user = User.objects.get(username=request.user.username)
+        word_list = RussianWord.objects.filter(author=user.id)
     except ObjectDoesNotExist:
         raise Http404("Russian Words do not exist")
     context = {
